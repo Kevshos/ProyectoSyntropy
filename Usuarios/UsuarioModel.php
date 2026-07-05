14c0648b1495fbbb7decad6963d40b246f78c275
Kevshos <kcarballo625@gmail.com>
2026-07-05 18:03:36 -0300

    Mapa Funcionando

--- Usuarios/UsuarioModel.php
+++ Usuarios/UsuarioModel.php
@@ -20,17 +20,16 @@ class UsuarioModel
     //Registrar usuario---------------------------------------------------
 
 
-    public function crearUsuario($n, $c, $a, $co, $m, $a2f){
-            $sql = "INSERT INTO Usuarios (CI, Nombre, Apellido, Contrasenia, Mail, A2F) VALUES (?,?,?,?,?,?)";
+    public function crearUsuario($n, $a, $co, $m, $a2f){
+            $sql = "INSERT INTO Usuarios (Nombre, Apellido, Contrasenia, Mail, A2F) VALUES (?,?,?,?,?)";
             $stmt = mysqli_prepare($this->conexion, $sql);
             $this->Nombre = $n;
-            $this->CI = $c;
             $this->Apellido = $a;
             $this->Contrasenia = password_hash($co, PASSWORD_DEFAULT);
             $this->Mail = $m;
             $this->A2F = $a2f;
 
-            $stmt->bind_param('issssi', $this->CI, $this->Nombre, $this->Apellido, $this->Contrasenia, $this->Mail, $this->A2F);
+            $stmt->bind_param('ssssi', $this->Nombre, $this->Apellido, $this->Contrasenia, $this->Mail, $this->A2F);
             if($stmt->execute()){
                 $stmt->close();
                 return true;
